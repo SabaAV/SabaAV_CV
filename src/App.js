d46d0e8e19365686f4b69045b2ecbbbd667ef4d2
AsterOH <avlokhashvili.saba@gmail.com>
2025-08-03 15:58:48 +0400

    Update 2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Navigation from "./Components/navigation";
 import About from "./Pages/About";
 import Awards from "./Pages/Awards";
 import Еxperience from "./Pages/Еxperience";
-import Projects from "./Pages/Projects";
+// import Projects from "./Pages/Projects";
 import Contacts from "./Pages/Contacts";
 
 function App() {
